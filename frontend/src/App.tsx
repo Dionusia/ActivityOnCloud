07c9e1e5a738c0a7d13d7718a734d4c0f5f548da
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-04-25 10:29:51 +0300

    Changed the page to be displayed when the server is running to the booking engine instead of app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import Dashboard from "./Components/Dashboard";
 import FilterComponents from "./Components/FilterCriteria";
+import BookingEngine from "./Pages/BookingEngine";
 const App: React.FC = () => {
   return (
     <div>
-      <FilterComponents />
+      <BookingEngine />
     </div>
   );
 };
