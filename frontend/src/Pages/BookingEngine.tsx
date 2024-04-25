51b2b58601791916b142ce530e08a963b2e9aeb5
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-04-25 12:45:51 +0300

    Added new interface for activity

--- frontend/src/Pages/BookingEngine.tsx
+++ frontend/src/Pages/BookingEngine.tsx
@@ -1,65 +1,91 @@
-import React from "react";
-import ActivityInfoParent from "../Components/ActivityInfo";
+import React, { useEffect, useState } from "react";
+import ActivityInfoParent, {type TimeSlot} from "../Components/ActivityInfo";
 import FilterComponents from "../Components/FilterCriteria";
+import instance from "../../config/AxiosConfig";
 
 const BookingEngine: React.FC = () => {
-    const timeSlots = [
-        {
-            "start": "10:00:00",
-            "end": "12:30:00"
-        },
-        {
-            "start": "12:30:00",
-            "end": "15:00:00"
-        },
-        {
-            "start": "15:00:00",
-            "end": "17:30:00"
-        }
-    ]
-    const activitiesList = [
-        {
-            "id": 1,
-            "activityAdmin": {
-                "id": 1
-            },
-            "name": "Title",
-            "description": "Lorem ipsus vwgwrbrwbrebrebrreberb rtbtrbtrbtrbtrbtrbtrb brtbrtbtrbtrbrtb brtbrtb bb rt",
-            "durationDays": 0,
-            "durationHours": 2,
-            "durationMinutes": 30,
-            "pricePerPerson": 100
-        },
-        {
-            "id": 51,
-            "activityAdmin": {
-                "id": 1
-            },
-            "name": "Title",
-            "description": "Lorem ipsus vwgwrbrwbrebrebrreberb rtbtrbtrbtrbtrbtrbtrb brtbrtbtrbtrbrtb brtbrtb bb rt",
-            "durationDays": 0,
-            "durationHours": 3,
-            "durationMinutes": 0,
-            "pricePerPerson": 200
-        },
-        {
-            "id": 101,
-            "activityAdmin": {
-                "id": 51
-            },
-            "name": "Title",
-            "description": "Lorem ipsus vwgwrbrwbrebrebrreberb rtbtrbtrbtrbtrbtrbtrb brtbrtbtrbtrbrtb brtbrtb bb rt",
-            "durationDays": 1,
-            "durationHours": 0,
-            "durationMinutes": 0,
-            "pricePerPerson": 300
-        }
-    ]
+    // const timeSlots = [
+    //     {
+    //         "start": "10:00:00",
+    //         "end": "12:30:00"
+    //     },
+    //     {
+    //         "start": "12:30:00",
+    //         "end": "15:00:00"
+    //     },
+    //     {
+    //         "start": "15:00:00",
+    //         "end": "17:30:00"
+    //     }
+    // ]
+    // const activitiesList = [
+    //     {
+    //         "id": 1,
+    //         "activityAdmin": {
+    //             "id": 1
+    //         },
+    //         "name": "Title",
+    //         "description": "Lorem ipsus vwgwrbrwbrebrebrreberb rtbtrbtrbtrbtrbtrbtrb brtbrtbtrbtrbrtb brtbrtb bb rt",
+    //         "durationDays": 0,
+    //         "durationHours": 2,
+    //         "durationMinutes": 30,
+    //         "pricePerPerson": 100
+    //     },
+    //     {
+    //         "id": 51,
+    //         "activityAdmin": {
+    //             "id": 1
+    //         },
+    //         "name": "Title",
+    //         "description": "Lorem ipsus vwgwrbrwbrebrebrreberb rtbtrbtrbtrbtrbtrbtrb brtbrtbtrbtrbrtb brtbrtb bb rt",
+    //         "durationDays": 0,
+    //         "durationHours": 3,
+    //         "durationMinutes": 0,
+    //         "pricePerPerson": 200
+    //     },
+    //     {
+    //         "id": 101,
+    //         "activityAdmin": {
+    //             "id": 51
+    //         },
+    //         "name": "Title",
+    //         "description": "Lorem ipsus vwgwrbrwbrebrebrreberb rtbtrbtrbtrbtrbtrbtrb brtbrtbtrbtrbrtb brtbrtb bb rt",
+    //         "durationDays": 1,
+    //         "durationHours": 0,
+    //         "durationMinutes": 0,
+    //         "pricePerPerson": 300
+    //     }
+    // ]
+    interface ActivityAdmin {
+        id: number;
+    }
+      
+    interface Activity {
+        id: number;
+        activityAdmin: ActivityAdmin;
+        name: string;
+        description: string;
+        durationDays: number;
+        durationHours: number;
+        durationMinutes: number;
+        pricePerPerson: number;
+    }
+    const [availableActivitiesList, setAvailableActivitiesList] = useState<Activity[]>([]);
+    useEffect(() => {
+        instance.get('/availability/available')
+            .then(response => {
+                setAvailableActivitiesList(response.data);
+            })
+            .catch( error => {
+                console.error('There was an error retrieving the timeSlots array' + error);
+            })
+    }); 
+    //TODO retrieve time slots for each activity
     return (
         <div className="flex flex-col space-y-4 items-center">
             <FilterComponents />
             <div className="flex flex-col items-center space-y-4">
-                {activitiesList.map((activity, index) => (
+                {availableActivitiesList.map((activity, index) => (
                     <div key={index} >
                         <ActivityInfoParent 
                             title={activity.name} 
