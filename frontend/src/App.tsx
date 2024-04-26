39cc9b7987657c1b319d3996fbbc2d9c7cec5590
m1kemp <mixalhs0mpallas@gmail.com>
2024-04-26 12:25:58 +0300

    app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+<<<<<<< Updated upstream
 import React from "react";
+=======
+import React, { useState } from "react";
+import FilterComponents from "./Components/FilterCriteria";
+>>>>>>> Stashed changes
 import BookingEngine from "./Pages/BookingEngine";
 import Dashboard from "./Pages/Dashboard";
 const App: React.FC = () => {
   return (
     <div>
+<<<<<<< Updated upstream
       <Dashboard />
+=======
+      <BookingEngine />
+>>>>>>> Stashed changes
     </div>
   );
 };
