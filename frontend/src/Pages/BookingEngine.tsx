7c76c42b1954eb0484b134a6e92522d83b57eb5d
GeorgeZarokanellos <geo.zarak@gmail.com>
2024-04-26 12:10:35 +0300

    Added function to create the list with available activities every time the search button is pressed

--- frontend/src/Pages/BookingEngine.tsx
+++ frontend/src/Pages/BookingEngine.tsx
@@ -2,57 +2,92 @@ import React, { useEffect, useState } from "react";
 import ActivityInfoParent from "../Components/ActivityInfo";
 import FilterComponents from "../Components/FilterCriteria";
 import instance from "../AxiosConfig";
+//#region interfaces and types
+interface ActivityAdmin {
+    id: number;
+}
+  
+interface Activity {
+    id: number;
+    activityAdmin: ActivityAdmin;
+    name: string;
+    description: string;
+    durationDays: number;
+    durationHours: number;
+    durationMinutes: number;
+    pricePerPerson: number;
+}
 
+
+type TimeSlot = {
+    start: string;
+    end: string;
+    remainingCapacity: string;
+}
+
+interface TimeSlots {
+    [key: string]: TimeSlot[];
+}
+//#endregion 
 const BookingEngine: React.FC = () => {
-    interface ActivityAdmin {
-        id: number;
-    }
-      
-    interface Activity {
-        id: number;
-        activityAdmin: ActivityAdmin;
-        name: string;
-        description: string;
-        durationDays: number;
-        durationHours: number;
-        durationMinutes: number;
-        pricePerPerson: number;
-    }
-    const [availableActivitiesList, setAvailableActivitiesList] = useState<Activity[]>([]);
+    const [activitiesList, setAvailableActivitiesList] = useState<Activity[]>([]);
+    const [timeSlots, setTimeSlots] = useState<TimeSlots>({});
+    const [renderKey, setRenderKey] = useState(0);
     useEffect(() => {
-        instance.get('/availability/available')
+        instance.get('/activity')
             .then(response => {
-                setAvailableActivitiesList(response.data);
+                setAvailableActivitiesList(response.data);                
             })
             .catch( error => {
-                console.error('There was an error retrieving the timeSlots array' + error);
+                console.error('There was an error retrieving the activities array' + error);
             })
-    }); 
-    //TODO retrieve time slots for each activity
-    return (
-        <div className="flex flex-col space-y-4 items-center">
-            <FilterComponents />
-            <div className="flex flex-col items-center space-y-4">
-                {availableActivitiesList.map((activity, index) => (
-                    <div key={index} >
+    },[]); 
+
+    useEffect(() => {
+        setRenderKey(prevKey => prevKey + 1);
+    }, [timeSlots]);
+
+    const createActivityInfoComponent = (activitiesList: Activity[], renderKey: number) => {
+        return activitiesList.map((activity, index) => {
+            const availableActivity = activitiesList.find(a => a.id === parseInt(Object.keys(timeSlots)[index]));
+            console.log(availableActivity);
+            
+            if(!availableActivity) {
+                return null;
+            }
+            return (
+                <div key={`${index}-${renderKey}`} >
+                    {Object.keys(timeSlots).length > 0 &&
                         <ActivityInfoParent 
-                            title={activity.name} 
-                            description={activity.description} 
-                            price={activity.pricePerPerson * 2} //TODO multiply by the number of people from filter component
-                            timeSlot= 
+                            title={availableActivity.name} 
+                            description={availableActivity.description} 
+                            price={availableActivity.pricePerPerson * 2} //TODO multiply by the number of people from filter component
+                            timeSlot={timeSlots[Object.keys(timeSlots)[index]]}
                             duration={
                                 {
-                                    durationDays: activity.durationDays,
-                                    durationHours: activity.durationHours,
-                                    durationMinutes: activity.durationMinutes
+                                    durationDays: availableActivity.durationDays,
+                                    durationHours: availableActivity.durationHours,
+                                    durationMinutes: availableActivity.durationMinutes
                                 }
                             } 
                         />
-                    </div>
-                ))}
+                    }
+                </div>)
+            })
+    }
+
+    return (
+        <div className="flex flex-col space-y-4 items-center">
+            <FilterComponents setTimeSlots={setTimeSlots}/>
+            
+            <div className="flex flex-col items-center space-y-4">
+                {    
+                    createActivityInfoComponent(activitiesList, renderKey)        
+                }
             </div>
         </div>
     );
 }
 
 export default BookingEngine;
+export type {TimeSlot,TimeSlots};
