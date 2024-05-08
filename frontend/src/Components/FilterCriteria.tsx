eaa76b79c976a2c322bc6d99db45205e267bf56d
m1kemp <mixalhs0mpallas@gmail.com>
2024-05-08 15:56:33 +0300

    authentication at filter criteria

--- frontend/src/Components/FilterCriteria.tsx
+++ frontend/src/Components/FilterCriteria.tsx
@@ -7,10 +7,14 @@ import { FilterComponentsProps } from "../InterfacesAndTypes/Interfaces";
 import { TimeSlots } from "../InterfacesAndTypes/Interfaces";
 import { TimeSlot } from "../InterfacesAndTypes/Types";
 
-
-
-const FilterComponents: React.FC<FilterComponentsProps> = ({setTimeSlots, selectedPerson ,setSelectedPerson, setFormattedDate}) => {
+const FilterComponents: React.FC<FilterComponentsProps> = ({
+  setTimeSlots,
+  selectedPerson,
+  setSelectedPerson,
+  setFormattedDate,
+}) => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [errorMessage, setErrorMessage] = useState("");
   let formattedTempDate = "test";
 
   const handleDateChange = (date: Date | null) => {
@@ -20,21 +24,33 @@ const FilterComponents: React.FC<FilterComponentsProps> = ({setTimeSlots, select
     setSelectedPerson(num);
   };
   const handleSearch = () => {
-    
+    if (!selectedDate && !selectedPerson) {
+      setErrorMessage("Please select a person and a date.");
+      return;
+    } else if (!selectedPerson) {
+      setErrorMessage("Please select a person.");
+      return;
+    } else if (!selectedDate) {
+      setErrorMessage("Please select a date.");
+      return;
+    }
+
+    setErrorMessage("");
+
     if (selectedDate && selectedPerson) {
       console.log("Selected Date:", selectedDate);
       console.log("Number of People:", selectedPerson);
     } else {
       console.log("Please select date and enter number of people");
     }
-    if( selectedDate === null || selectedPerson === null){ 
-      console.log("Please select date and enter number of people"); return;
-    }
-    else{
+    if (selectedDate === null || selectedPerson === null) {
+      console.log("Please select date and enter number of people");
+      return;
+    } else {
       let offset = selectedDate.getTimezoneOffset();
-      let adjustedDate = new Date(selectedDate.getTime() - (offset*60*1000));
-      setFormattedDate(adjustedDate.toISOString().split('T')[0]);
-      formattedTempDate = adjustedDate.toISOString().split('T')[0];
+      let adjustedDate = new Date(selectedDate.getTime() - offset * 60 * 1000);
+      setFormattedDate(adjustedDate.toISOString().split("T")[0]);
+      formattedTempDate = adjustedDate.toISOString().split("T")[0];
       console.log("Formatted Date:", formattedTempDate);
     }
     instance
@@ -45,17 +61,20 @@ const FilterComponents: React.FC<FilterComponentsProps> = ({setTimeSlots, select
         },
       })
       .then((response) => {
-        console.log("Time slots:", response.data); 
-        const modifiedResponseData = Object.keys(response.data).reduce((acc, key) => {
-          acc[key] = response.data[key].map((timeSlot: TimeSlot) => ({
-            ...timeSlot,
-            start: timeSlot.start.split('T')[1],
-            end: timeSlot.end.split('T')[1],
-          }));
-          return acc;
-        }, {} as TimeSlots);
-      
-        setTimeSlots(modifiedResponseData);        
+        console.log("Time slots:", response.data);
+        const modifiedResponseData = Object.keys(response.data).reduce(
+          (acc, key) => {
+            acc[key] = response.data[key].map((timeSlot: TimeSlot) => ({
+              ...timeSlot,
+              start: timeSlot.start.split("T")[1],
+              end: timeSlot.end.split("T")[1],
+            }));
+            return acc;
+          },
+          {} as TimeSlots
+        );
+
+        setTimeSlots(modifiedResponseData);
       })
       .catch((error) => {
         console.log(error + ": Get time slots error");
@@ -75,6 +94,9 @@ const FilterComponents: React.FC<FilterComponentsProps> = ({setTimeSlots, select
         />
         <SearchButton onSearch={handleSearch} />
       </div>
+      {errorMessage && (
+        <div className="mt-4 text-center text-red-500">{errorMessage}</div>
+      )}
     </form>
   );
 };
