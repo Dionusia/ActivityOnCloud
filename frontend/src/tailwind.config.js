2b724cd7af2cf364371f559e97f90d773aac4113
Manos Loukakis <manolouk2010@gmail.com>
2024-05-10 15:15:58 +0300

    fonts installed

--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
 
   theme: {
     extend: {
+      fontFamily: {
+        'roboto-slab': ['Roboto Slab', 'serif'],
+        'helvetica': ['Helvetica', 'sans-serif'],
+        'myriad': ['Myriad Pro', 'sans-serif']
+      },
       colors: {
         'customGreen': '#04B1A7',
         'customHoverGreen': '#03857D'
