e55e2acd6c4c2a2655c6b5275f94861565b15266
ptriantafy <pantrian04@gmail.com>
2024-05-10 10:38:49 +0300

    merged develop

--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import BookingsTable from '../Components/BookingsTable';
-import { Booking } from '../InterfacesAndTypes/Types';
-import instance from '../AxiosConfig';
-
+import React, { useEffect, useState } from "react";
+import BookingsTable from "../Components/BookingsTable";
+import { Booking } from "../InterfacesAndTypes/Types";
+import { ActivityOption } from "../InterfacesAndTypes/Types";
+import instance from "../AxiosConfig";
+import ActivityOptionTable from "../Components/ActivityOptionTable";
+import SearchByName from "../Components/SearchByName";
 
 const Dashboard: React.FC = () => {
   const [bookingsList, setBookingsList] = useState<Booking[]>([]);
+  const [activityOpts, setActivityOptions] = useState<ActivityOption[]>([]);
 
   useEffect(() => {
     instance
@@ -13,11 +16,13 @@ const Dashboard: React.FC = () => {
       .then((response) => {
         const formattedBookings: Booking[] = response.data.map(
           (bookingData: any) => ({
-            customerName: bookingData.customerName,
-            activityName: bookingData.activity.name,
+            id: bookingData.uuid,
+            customerName: bookingData.name +" "+ bookingData.surname,
+            contact: bookingData.email+" | "+bookingData.phone,
+            activityName: bookingData.activityOption.name,
             participantsNum: bookingData.persons,
-            timeframe: formatDateTime(bookingData.startTime),
-            pricePayed: bookingData.priceTotal,
+            timeframe: bookingData.startTime,
+            pricePayed: bookingData.totalPrice,
           })
         );
         setBookingsList(formattedBookings);
@@ -27,6 +32,24 @@ const Dashboard: React.FC = () => {
       });
   }, []);
 
+  useEffect(() => {
+    instance
+      .get("/activity-option")
+      .then((response) => {
+        const activityOptions: ActivityOption[] = response.data.map(
+          (activityData: any) => ({
+            activityName: activityData.name,
+            activityDescription: activityData.description,
+            activityDuration: activityData.duration,
+            activityCapacity: activityData.capacity,
+          })
+        );
+        setActivityOptions(activityOptions);
+      })
+      .catch((error) => {
+        console.error("Error fetching activity options:", error);
+      });
+  }, []);
   // Function to format date and time
   const formatDateTime = (dateTimeString: string): string => {
     const dateTime = new Date(dateTimeString);
@@ -39,10 +62,16 @@ const Dashboard: React.FC = () => {
   };
 
   return (
-    <div className='flex flex-col '>
-      <h1 className='flex-grow text-center my-6 py-2 px-4 shadow-md rounded'>Dashboard</h1>
-      <div className='text-center'> Bookings
-        <BookingsTable booking={bookingsList} />
+    <div className="flex flex-col ">
+      <h1 className="flex-grow text-center my-6 py-2 px-4 shadow-md rounded">
+        Dashboard
+      </h1>
+      <div className="text-center">
+        Bookings
+         <SearchByName booking={bookingsList} />
+         <br/>
+         Activities
+        <ActivityOptionTable activityOption={activityOpts} />
       </div>
     </div>
   );
