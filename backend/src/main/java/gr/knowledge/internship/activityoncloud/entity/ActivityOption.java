03b6c41013c422bd5d0a5179c9499a96afede92d
ptriantafy <pantrian04@gmail.com>
2024-05-10 14:27:33 +0300

    code format and cleanup

--- backend/src/main/java/gr/knowledge/internship/activityoncloud/entity/ActivityOption.java
+++ backend/src/main/java/gr/knowledge/internship/activityoncloud/entity/ActivityOption.java
@@ -3,13 +3,23 @@ package gr.knowledge.internship.activityoncloud.entity;
 import java.io.Serializable;
 import java.time.Duration;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import jakarta.persistence.*;
+import gr.knowledge.internship.activityoncloud.converter.DurationConverter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.*;
-import org.hibernate.annotations.Type;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -18,28 +28,30 @@ import org.hibernate.annotations.Type;
 @NoArgsConstructor
 @Table(name = "activity_option")
 public class ActivityOption implements Serializable {
-    @Id
-    @Column(name = "id")
-    @NotNull
-    @GeneratedValue(generator = "option_seq", strategy = GenerationType.SEQUENCE)
-    @SequenceGenerator(name = "option_seq", sequenceName = "option_seq")
-    private Long id;
-    @ManyToOne
-    @JoinColumn(name = "activity_id")
-    @NotNull
-    private Activity activity;
-    @Column(name = "name", length = 64)
-    @Size(max = 64)
-    @NotNull
-    private String name;
-    @Column(name = "description", length = 256)
-    @Size(max = 256)
-    private String description;
-    @Column(name = "duration")
-    @NotNull
-    @Convert(converter = DurationConverter.class)
-    private Duration duration;
-    @Column(name = "capacity")
-    @NotNull
-    private Long capacity;
+	@Id
+	@Column(name = "id")
+	@NotNull
+	@GeneratedValue(generator = "option_seq", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "option_seq", sequenceName = "option_seq")
+	private Long id;
+	@ManyToOne
+	@JoinColumn(name = "activity_id")
+	@NotNull
+	private Activity activity;
+	@Column(name = "name", length = 64)
+	@Size(max = 64)
+	@NotNull
+	private String name;
+	@Column(name = "description", length = 256)
+	@Size(max = 256)
+	private String description;
+	@Column(name = "duration")
+	@NotNull
+	@Convert(converter = DurationConverter.class)
+	private Duration duration;
+	@Column(name = "capacity")
+	@NotNull
+	private Long capacity;
+	@Column(name = "imageURL")
+	private String imageURL;
 }
--- backend/src/main/java/gr/knowledge/internship/activityoncloud/repository/ActivityOptionRepository.java
+++ backend/src/main/java/gr/knowledge/internship/activityoncloud/repository/ActivityOptionRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import gr.knowledge.internship.activityoncloud.entity.ActivityOption;
 
 public interface ActivityOptionRepository extends JpaRepository<ActivityOption, Long> {
+
 }
