cb3030dbd9050ff8a48f55e23c7116a8f7085ef2
m1kemp <mixalhs0mpallas@gmail.com>
2024-05-14 12:49:05 +0300

    Activity Creation change

--- frontend/src/Pages/ActivityCreation.tsx
+++ frontend/src/Pages/ActivityCreation.tsx
@@ -11,21 +11,21 @@ const ActivityCreation: React.FC = () => {
     setSelectedPerson(num);
   };
   return (
-    <Formik
-      initialValues={{ name: "", surname: "", email: "", phone: "" }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form className="flex-col items-center">
-          <div className="flex flex-wrap p-1">
-            <div className="w-1/2 flex">
+    <div className="flex justify-center w-8/10">
+      <Formik
+        initialValues={{ name: "", surname: "", email: "", phone: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="">
+            <div className="flex  w-full  m-2 p-1">
               <Field
-                className=" md:w-1/3 flex-grow m-2  rounded-lg focus:ring-customGreen focus:border-customGreen"
+                className="flex-1 m-2  rounded-lg focus:ring-customGreen focus:border-customGreen"
                 type="text"
                 name="name"
                 placeholder="Title"
@@ -34,7 +34,7 @@ const ActivityCreation: React.FC = () => {
               <Field
                 as="select"
                 name="category"
-                className=" md:w-1/3 flex-1 m-2 rounded-lg focus:ring-customGreen focus:border-customGreen"
+                className="flex-1 m-2 rounded-lg focus:ring-customGreen focus:border-customGreen"
               >
                 <option value="">Category</option>
                 <option value="category1">WaterSports</option>
@@ -42,58 +42,55 @@ const ActivityCreation: React.FC = () => {
                 <option value="category3">Yoga</option>
               </Field>
             </div>
-            <div className="w-1/2 ">
-              <Field
-                type="number"
-                name="capacity"
-                placeholder="People"
-                className=" w-1/4 flex-1 m-2 rounded-lg bg-white  focus:ring-customGreen focus:border-customGreen"
-              />
-            </div>
-          </div>
 
-          <div className="flex flex-wrap   md:flex-wrap p-1">
-            <Field
-              as="textarea"
-              name="description"
-              placeholder="Description"
-              className="w-1/4 flex-1 m-2 rounded-lg bg-white focus:ring-customGreen focus:border-customGreen"
-              style={{ maxHeight: "95px" }}
-            />
-            <div className="flex flex-col w-1/2">
+            <div className="flex m-2 p-1  ">
               <Field
-                type="number"
-                name="price"
-                placeholder="Price"
-                className=" w-1/4 flex-1 m-2 rounded-lg bg-white h-1/4 focus:ring-customGreen focus:border-customGreen"
+                as="textarea"
+                name="description"
+                placeholder="Description"
+                className="w-1/2 flex-1 m-2 rounded-lg bg-white focus:ring-customGreen focus:border-customGreen"
+                style={{ maxHeight: "140px" }}
               />
-              <Field
-                type="number"
-                name="duration"
-                placeholder="Duration"
-                min="0"
-                className="w-1/4 flex-1 m-2 rounded-lg bg-white h-1/4 focus:ring-customGreen focus:border-customGreen"
-              />
+              <div className="flex flex-col  items-center">
+                <Field
+                  type="number"
+                  name="price"
+                  placeholder="Price"
+                  className=" w-1/2  flex-1 m-2 rounded-lg bg-white h-1/4 focus:ring-customGreen focus:border-customGreen"
+                />
+                <Field
+                  type="number"
+                  name="duration"
+                  placeholder="Duration"
+                  min="0"
+                  className="w-1/2 flex-1 m-2 rounded-lg bg-white h-1/4 focus:ring-customGreen focus:border-customGreen"
+                />
+                <Field
+                  type="number"
+                  name="capacity"
+                  placeholder="People"
+                  className=" w-1/2 flex-1 m-2 rounded-lg bg-white  h-1/4 focus:ring-customGreen focus:border-customGreen"
+                />
+              </div>
             </div>
-          </div>
-          <div className="flex flex-wrap justify-between p-1">
-            <Field
-              type="file"
-              name="photo"
-              className="w-full md:w-1/4 flex-1 m-2 rounded-lg bg-white"
-            />
-          </div>
-          <div className="flex  w-1/2 justify-center">
-            <Button
-              className="w-full md:w-1/2 flex-1 m-2 bg-customGreen text-white rounded-lg hover:bg-customGreen-dark"
-              type="submit"
-            >
-              Submit
-            </Button>
-          </div>
-        </Form>
-      )}
-    </Formik>
+            <div className="flex flex-col items-center">
+              <Field
+                type="file"
+                name="photo"
+                className="w-1/2 flex-1 m-2 rounded-lg bg-white"
+              />
+
+              <Button
+                className="w-8/10  md:w-1/2 flex-1 m-2  bg-customGreen text-white rounded-lg hover:bg-customGreen-dark"
+                type="submit"
+              >
+                Submit
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 };
 export default ActivityCreation;
