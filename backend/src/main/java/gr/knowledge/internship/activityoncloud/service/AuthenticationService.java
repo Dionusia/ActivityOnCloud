f2faece05eb44a99a95971d15ab022c5b133601e
ptriantafy <pantrian04@gmail.com>
2024-05-15 12:53:22 +0300

    implemented new activity_admin insertion after signup

--- backend/src/main/java/gr/knowledge/internship/activityoncloud/service/AuthenticationService.java
+++ backend/src/main/java/gr/knowledge/internship/activityoncloud/service/AuthenticationService.java
@@ -1,5 +1,6 @@
 package gr.knowledge.internship.activityoncloud.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -7,17 +8,21 @@ import org.springframework.stereotype.Service;
 
 import gr.knowledge.internship.activityoncloud.dto.LoginUserDTO;
 import gr.knowledge.internship.activityoncloud.dto.RegisterUserDTO;
+import gr.knowledge.internship.activityoncloud.entity.ActivityAdmin;
 import gr.knowledge.internship.activityoncloud.entity.User;
+import gr.knowledge.internship.activityoncloud.mapper.ActivityAdminMapper;
 import gr.knowledge.internship.activityoncloud.repository.UserRepository;
 
 @Service
 public class AuthenticationService {
 	private final UserRepository userRepository;
-
 	private final PasswordEncoder passwordEncoder;
-
 	private final AuthenticationManager authenticationManager;
-
+	@Autowired
+	private ActivityAdminService activityAdminService;
+	@Autowired
+	private ActivityAdminMapper activityAdminMapper;
+	
 	public AuthenticationService(UserRepository userRepository, AuthenticationManager authenticationManager,
 			PasswordEncoder passwordEncoder) {
 		this.authenticationManager = authenticationManager;
@@ -27,10 +32,14 @@ public class AuthenticationService {
 
 	public User signup(RegisterUserDTO input) {
 		User user = new User();
+		ActivityAdmin admin = new ActivityAdmin();
 		user.setEmail(input.getEmail());
 		user.setPassword(passwordEncoder.encode(input.getPassword()));
 		user.setFullName(input.getFullName());
-		return userRepository.save(user);
+		admin.setUser(user);
+		userRepository.save(user);
+		activityAdminService.saveActivityAdmin(activityAdminMapper.toDTO(admin));
+		return user;
 	}
 
 	public User authenticate(LoginUserDTO input) {
