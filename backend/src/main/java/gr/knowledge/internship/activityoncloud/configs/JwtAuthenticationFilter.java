7d310cef47a7176e08647cd627d7fe4839caa379
ptriantafy <pantrian04@gmail.com>
2024-05-22 10:43:19 +0300

    try/catch ExpiredJwtException

--- backend/src/main/java/gr/knowledge/internship/activityoncloud/configs/JwtAuthenticationFilter.java
+++ backend/src/main/java/gr/knowledge/internship/activityoncloud/configs/JwtAuthenticationFilter.java
@@ -2,29 +2,28 @@ package gr.knowledge.internship.activityoncloud.configs;
 
 import java.io.IOException;
 
-import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import gr.knowledge.internship.activityoncloud.service.JwtService;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-@Log4j2
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
+	@Autowired
 	private final JwtService jwtService;
+	@Autowired
 	private final UserDetailsService userDetailsService;
 
 	public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
@@ -33,8 +32,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	}
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-									FilterChain filterChain) throws ServletException, IOException {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException, ExpiredJwtException {
 		final String authHeader = request.getHeader("Authorization");
 		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
 			filterChain.doFilter(request, response);
@@ -42,32 +41,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		}
 		final String jwt = authHeader.substring(7);
 		final String userEmail = jwtService.extractUsername(jwt);
+		try {
+			jwtService.isTokenExpired(jwt);
 
-		// Check if token is expired
-		if (jwtService.isTokenExpired(jwt)) {
-			int statusCode = HttpStatus.UNAUTHORIZED.value();
-			response.setStatus(statusCode);
-			log.debug("Token has expired. Sending status code: {}", statusCode);
+		} catch (ExpiredJwtException e) {
+			response.setStatus(HttpStatus.UNAUTHORIZED.value());
 			return;
 		}
-
-		// Check if token is valid
 		UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-		if (!jwtService.isTokenValid(jwt, userDetails)) {
-			int statusCode = HttpStatus.UNAUTHORIZED.value();
-			response.setStatus(statusCode);
-			log.debug("Invalid token. Sending status code: {}", statusCode);
-			return;
-		}
-
-		// Authenticate user if token is valid
 		if (jwtService.isTokenValid(jwt, userDetails)) {
-			Authentication authToken = new UsernamePasswordAuthenticationToken(userDetails,
-					null, userDetails.getAuthorities());
+			Authentication authToken = new UsernamePasswordAuthenticationToken(userDetails, null,
+					userDetails.getAuthorities());
 			SecurityContextHolder.getContext().setAuthentication(authToken);
 		}
-
 		filterChain.doFilter(request, response);
 	}
-
 }
